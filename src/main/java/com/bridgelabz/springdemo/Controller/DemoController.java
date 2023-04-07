ed7dc7a05cb159b9eceebe28cd34b26b8a57f566
unknown <gownideepu23@gmail.com>
2023-04-07 13:18:38 +0530

    added

--- src/main/java/com/bridgelabz/springdemo/Controller/DemoController.java
+++ src/main/java/com/bridgelabz/springdemo/Controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.springdemo.Controller;
 
+import com.bridgelabz.springdemo.Model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,5 +18,9 @@ public class DemoController {
     @RequestMapping(value = "/path/{message}", method = RequestMethod.GET)
     public String message2(@PathVariable String message) {
         return message;
+    }
+    @RequestMapping(value = "/body", method = RequestMethod.POST)
+    public String acceptData(@RequestBody User user) {
+    return "FirstName: " +user.getFirstName() + "LastName: " +user.getLastName();
     }
 }
---
+++ src/main/java/com/bridgelabz/springdemo/Model/User.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.springdemo.Model;
+
+public class User {
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+
+}
