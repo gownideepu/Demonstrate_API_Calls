d4cfd1fd7ff8a48d8225f25fa0eeac276e33c681
unknown <gownideepu23@gmail.com>
2023-04-07 11:52:14 +0530

    added

--- src/main/java/com/bridgelabz/springdemo/Controller/DemoController.java
+++ src/main/java/com/bridgelabz/springdemo/Controller/DemoController.java
@@ -1,19 +1,21 @@
 package com.bridgelabz.springdemo.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DemoController {
-    @RequestMapping(value = "/message",method = RequestMethod.GET)
-    public String message(){
+    @RequestMapping(value = "/message", method = RequestMethod.GET)
+    public String message() {
         return "Hello World";
     }
-    @RequestMapping(value = "/queryparam",method = RequestMethod.GET)
-    public String massage1(@RequestParam String message){
+
+    @RequestMapping(value = "/queryparam", method = RequestMethod.GET)
+    public String message1(@RequestParam String message) {
         return message;
     }
 
+    @RequestMapping(value = "/path/{message}", method = RequestMethod.GET)
+    public String message2(@PathVariable String message) {
+        return message;
+    }
 }
